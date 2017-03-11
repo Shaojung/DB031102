26168177eeaaeba41a85e4c17fc282d67bd8b8ac
Shaojung Lu <shaojung@gmail.com>
2017-03-11 10:48:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/teacher/db031102/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.teacher.db031102;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void clickA(View v)
+    {
+        // ft.replace(R.id.contentLayout, new AFragment(), "f_a");
+        ft.add(R.id.contentLayout, new AFragment());
+
+    }
+    public void clickB(View v)
+    {
+        ft.replace(R.id.contentLayout, new BFragment(), "f_b");
+    }
+}
